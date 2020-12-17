3032a524fffd35049760d59c6df5b72a3e2f41df
Tomás Muñoz <tomas.mpoupin@gmail.com>
2020-12-17 15:38:53 -0300

    Finished means study

--- playground/tomas/analysis/utils.py
+++ playground/tomas/analysis/utils.py
@@ -45,3 +45,75 @@ def plot_diff_distance(method, dataset, diff_dataset, xlim, ylim):
     ax.axis
     plt.xlim(xlim)
     plt.ylim(ylim)
+
+def plot_means(dataset, title, xlim, ylim, xlabel, ylabel):
+    groups = pd.unique(dataset["group_num"])
+    colors = []
+    for i in groups:
+        colors.append(np.random.rand(3,))
+    color = []
+    for index, value in dataset["group_num"].iteritems():
+        color.append(colors[value])
+
+    fig, ax = plt.subplots()
+    fig.set_figwidth(15)
+    fig.set_figheight(10)
+
+    ax.scatter(dataset["mean_0"], dataset["mean_1"],
+        c=color, s=50)
+
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.axis
+    ax.grid(True)
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+
+def plot_stdDev(dataset, title, xlim, ylim, xlabel, ylabel):
+    groups = pd.unique(dataset["group_num"])
+    colors = []
+    for i in groups:
+        colors.append(np.random.rand(3,))
+    color = []
+    for index, value in dataset["group_num"].iteritems():
+        color.append(colors[value])
+
+    fig, ax = plt.subplots()
+    fig.set_figwidth(15)
+    fig.set_figheight(10)
+
+    ax.scatter(dataset["stddev_0"], dataset["stddev_1"],
+        c=color, s=50)
+
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.axis
+    ax.grid(True)
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+
+def plot_generic(dataset, x, y, title, xlim, ylim, xlabel, ylabel):
+    groups = pd.unique(dataset["group_num"])
+    colors = []
+    for i in groups:
+        colors.append(np.random.rand(3,))
+    color = []
+    for index, value in dataset["group_num"].iteritems():
+        color.append(colors[value])
+
+    fig, ax = plt.subplots()
+    fig.set_figwidth(15)
+    fig.set_figheight(10)
+
+    ax.scatter(x, y,
+        c=color, s=50)
+
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.axis
+    ax.grid(True)
+    plt.xlim(xlim)
+    plt.ylim(ylim)
